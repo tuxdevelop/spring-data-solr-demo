42c401c8e5ddc0a1de9ff74a2bde3407d89c1d86
tuxdevelop <tuxdevelop@gmail.com>
2015-07-05 19:33:51 +0200

    refactoring

--- spring-data-solr-demo-data-rest/src/test/java/org/tuxdevelop/spring/data/solr/demo/SolrDataRestApplicationIT.java
+++ spring-data-solr-demo-data-rest/src/test/java/org/tuxdevelop/spring/data/solr/demo/SolrDataRestApplicationIT.java
@@ -1,6 +1,5 @@
 package org.tuxdevelop.spring.data.solr.demo;
 
-import lombok.extern.slf4j.Slf4j;
 import org.junit.Before;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,20 +10,18 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 import org.tuxdevelop.spring.data.solr.demo.configuration.ITConfiguration;
 
-@Slf4j
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringApplicationConfiguration(classes = ITConfiguration.class)
 @WebAppConfiguration
 @IntegrationTest("server.port:0")
 public class SolrDataRestApplicationIT extends ITTests {
 
+	@Autowired
+	private EmbeddedWebApplicationContext server;
 
-    @Autowired
-    private EmbeddedWebApplicationContext server;
-
-    @Before
-    public void init() {
-        baseUrl = HTTP_LOCALHOST + server.getEmbeddedServletContainer().getPort() + DATA_SOLR_STORES;
-    }
+	@Before
+	public void init() {
+		baseUrl = HTTP_LOCALHOST + server.getEmbeddedServletContainer().getPort() + DATA_SOLR_STORES;
+	}
 
 }
--- spring-data-solr-demo-data-rest/src/test/java/org/tuxdevelop/spring/data/solr/demo/SolrDataRestApplicationSystemIT.java
+++ spring-data-solr-demo-data-rest/src/test/java/org/tuxdevelop/spring/data/solr/demo/SolrDataRestApplicationSystemIT.java
@@ -1,20 +1,18 @@
 package org.tuxdevelop.spring.data.solr.demo;
 
-import lombok.extern.slf4j.Slf4j;
 import org.junit.Before;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.SpringApplicationConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.tuxdevelop.spring.data.solr.demo.configuration.SystemITConfiguration;
 
-@Slf4j
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringApplicationConfiguration(classes = SystemITConfiguration.class)
 public class SolrDataRestApplicationSystemIT extends ITTests {
 
-    @Before
-    public void init() {
-        baseUrl = HTTP_LOCALHOST + "9010" + DATA_SOLR_STORES;
-    }
+	@Before
+	public void init() {
+		baseUrl = HTTP_LOCALHOST + "9010" + DATA_SOLR_STORES;
+	}
 
 }
